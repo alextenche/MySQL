d3c8dc1b90ee14539d5b8917c9ac70a9a0486429
Alex Tenche <alex.tenche@gmail.com>
2016-06-12 19:32:52 +0300

    added db

---
+++ db/game.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 12, 2016 at 07:30 PM
+-- Server version: 5.6.17
+-- PHP Version: 7.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `game`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `first_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `last_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `first_name`, `last_name`, `email`, `last_update`) VALUES
+(1, 'alexTenche', 'Alex', 'Tenche', 'alex.tenche@gmail.com', '2016-06-12 04:41:25'),
+(2, 'dayBlade', 'Blade', 'Daywalker', 'blade.daywalker@gmail.com', '2016-06-07 07:33:10'),
+(3, 'Alexone', 'Alexone', 'Italiano', 'alexoneIT@gmail.com', '2016-06-01 06:17:48'),
+(4, 'henry', 'Henry', 'Belafonte', 'henry.belafonte@yahoo.com', '2016-06-09 05:20:25'),
+(5, 'warcraft', 'War', 'Craft', 'war.craft@gmail.com', '2016-05-11 11:44:43'),
+(6, 'bladeNight', 'Blade', 'Nightwalker', 'blade.nightwalker@gmail.com', '2016-06-02 15:29:35');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `last_name_idx` (`last_name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- ordering.sql
+++ ordering.sql
@@ -8,4 +8,5 @@ SELECT * FROM `users` ORDER BY `lastName` ASC, `firstName` ASC LIMIT 5;
 SELECT * FROM `users` ORDER BY `lastName` ASC, `firstName` ASC LIMIT 5 OFFSET 5;
 SELECT * FROM `users` ORDER BY `lastName` ASC, `firstName` ASC LIMIT 5, 5;
 
-SELECT TOP 5 `lastName` FROM `people`;
+# `first_name` ASC, `last_name` DESC
+SELECT `id`, `first_name`, `last_name` FROM `users` WHERE id > 1 ORDER BY `first_name`, `last_name` DESC;
